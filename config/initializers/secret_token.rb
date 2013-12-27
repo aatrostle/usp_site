d75445f231dc0ff034adae542c4ca90f1fb142cb
Aaron Trostle <aaron.trostle@gmail.com>
2013-12-26 21:40:23 -0800

    new rails 4 app with postgres and without test unit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UspSite::Application.config.secret_key_base = 'cf548b2026ff9f6192e6aea3b50f9735b540ae0524dc5e5d9af0829af13f54bf1c73e858e08e76ffa2460f64c85c60c21ce43b0231c6d76a60cd11723438bf0d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UspSite::Application.config.session_store :cookie_store, key: '_usp_site_session'
